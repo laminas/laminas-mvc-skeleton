271d30a7e0af865cfcc4ed9ebb1756038c4bd43b
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-09-01 20:41:03 -0300

    Update Module.php
    Unneccesary variable assignment
    
    Signed-off-by: Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -9,7 +9,6 @@ class Module
     public function getConfig(): array
     {
         /** @var array $config */
-        $config = include __DIR__ . '/../config/module.config.php';
-        return $config;
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
